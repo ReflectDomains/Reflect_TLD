6bad4161afe1c2406e02f3e7d2d98efab389daaa
npcKei <sk11xg@qq.com>
2023-05-05 21:44:23 +0800

    feat:Add plugins

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,29 @@
+export const zeroAddress = '0x0000000000000000000000000000000000000000';
+
 export const splitAddress = (address, start = 5, end = -4) => {
-  return (
-    (address && address.slice(0, start) + "..." + address.slice(end)) || ""
-  );
+	return (
+		(address && address.slice(0, start) + '...' + address.slice(end)) || ''
+	);
+};
+
+export const throttle = (fn, delay) => {
+	let throttleTimer = null;
+	return function () {
+		if (throttleTimer) return;
+		throttleTimer = setTimeout(() => {
+			fn.apply(this, arguments);
+			throttleTimer = null;
+		}, delay);
+	};
+};
+
+export const debounce = (fn, delay) => {
+	let debounceTimer = null;
+	return function () {
+		if (debounceTimer) clearTimeout(debounceTimer);
+		debounceTimer = setTimeout(() => {
+			fn.apply(this, arguments);
+			debounceTimer = null;
+		}, delay);
+	};
 };
